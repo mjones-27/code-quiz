c921e88f835eb824f67ed291f42b9c575f736d4e
unknown <mars.jones34@gmail.com>
2020-04-01 00:15:31 -0700

    code runs without breaking. no logic yet

--- script.js
+++ script.js
@@ -1,66 +1,57 @@
-// The array of questions for our quiz game.
-// var questions = [
-//     { q: "The sky is blue.", a: "t" },
-//     { q: "There are 365 days in a year.", a: "t" },
-//     { q: "There are 42 ounces in a pound.", a: "f" },
-//     { q: "The Declaration of Independence was created in 1745.", a: "f" },
-//     { q: "Bananas are vegetables.", a: "f" }
-//   ];
-
-  // We start the game with a score of 0.
-//   var score = 0;
-
-  // Loop over every question object
-//   for (var i = 0; i < questions.length; i++) {
-    // Display current question to user and ask OK/Cancel
-    // var answer = confirm(questions[i].q);
-
-    // Compare answers
-    // if ((answer === true && questions[i].a === "t") ||
-    //   (answer === false && questions[i].a === "f")) {
-      // Increase score
-//       score++;
-//       alert("Correct!");
-//     }
-//     else {
-//       alert("Wrong!");
-//     }
-//   }
-
-  // Show total at end
-//   alert("You got " + score + "/" + questions.length);
-
-
-// PHASE 1
-
-// PHASE 2
-// TODO: display 5 sets of 4 questions
-// TODO: start timer
-
-
-// PHASE 3
-
-// HIGHSCORE
-
-// var targetDiv = document.getElementById("high");
-
 var starCard = document.getElementById("startCard")
 var timer = document.getElementById("timer")
 
 function quizTimer() {
     var secLeft = 75;
-
     var timerInterval = setInterval(function(){
         secLeft--;
         timer.textContent = "time: " + secLeft;
     }, 1000)                            // * 1000 because they are in ms
 }
 
-$(".start-button").on("click", function() {
-    // console.log("button pressed");
-    quizTimer();
+$(".start-button").on("click", function() {    quizTimer();
     startCard.classList.add("hide");
     quizCard1.classList.remove("hide");
+  });
 
+  $(".set1").on("click", function() { 
+    quizCard1.classList.add("hide");
+    quizCard2.classList.remove("hide");
+  });
+
+  $(".set2").on("click", function() {
+       
+    quizCard2.classList.add("hide");
+    quizCard3.classList.remove("hide");
+  });
+
+  $(".set3").on("click", function() {    
+    quizCard3.classList.add("hide");
+    quizCard4.classList.remove("hide");
+  });
+
+  $(".set4").on("click", function() {    
+    quizCard4.classList.add("hide");
+    quizCard5.classList.remove("hide");
+  });
+
+  $(".set5").on("click", function() {    
+    quizCard5.classList.add("hide");
+    endCard.classList.remove("hide");
+  });
+
+  $(".submit-button").on("click", function() {    
+    endCard.classList.add("hide");
+    highCard.classList.remove("hide");
+  });
+
+  $(".back-button").on("click", function() {    
+    highCard.classList.add("hide");
+    startCard.classList.remove("hide");
+  });
+
+  $(".clear-button").on("click", function() {    
+    endCard.classList.add("hide");
+    highCard.classList.remove("hide");
   });
 
