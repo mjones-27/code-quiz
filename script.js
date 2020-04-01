135cc43a2408c3a88ce7594ed0aa1fc6b8864d0b
unknown <mars.jones34@gmail.com>
2020-03-31 21:44:03 -0700

    first page up - no functionality

---
+++ script.js
@@ -0,0 +1,51 @@
+// The array of questions for our quiz game.
+// var questions = [
+//     { q: "The sky is blue.", a: "t" },
+//     { q: "There are 365 days in a year.", a: "t" },
+//     { q: "There are 42 ounces in a pound.", a: "f" },
+//     { q: "The Declaration of Independence was created in 1745.", a: "f" },
+//     { q: "Bananas are vegetables.", a: "f" }
+//   ];
+
+  // We start the game with a score of 0.
+//   var score = 0;
+
+  // Loop over every question object
+//   for (var i = 0; i < questions.length; i++) {
+    // Display current question to user and ask OK/Cancel
+    // var answer = confirm(questions[i].q);
+
+    // Compare answers
+    // if ((answer === true && questions[i].a === "t") ||
+    //   (answer === false && questions[i].a === "f")) {
+      // Increase score
+//       score++;
+//       alert("Correct!");
+//     }
+//     else {
+//       alert("Wrong!");
+//     }
+//   }
+
+  // Show total at end
+//   alert("You got " + score + "/" + questions.length);
+
+
+// PHASE 1
+
+// PHASE 2
+
+
+
+// PHASE 3
+
+// HIGHSCORE
+
+// var targetDiv = document.getElementById("high");
+
+$(".start-button").on("click", function() {
+    console.log("button pressed");
+    // var targetDiv = document.getElementById("cardTitle");
+    // targetDiv.setAttribute("class", "fancy");
+  });
+
